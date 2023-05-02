eeb5347fa844f297e0229c9d4e5e766d1bbd5ccc
mohpython <mohammedtraore301@gmail.com>
2023-05-02 17:33:27 +0000

    first commit

---
+++ iqra/iqra/user_login.py
@@ -0,0 +1,80 @@
+
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from app.EmailBackEnd import EmailBackEnd
+
+
+def REGISTER(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+
+        # verifier email
+        if User.objects.filter(email=email).exists():
+           messages.warning(request,'Email Existe déjà !')
+           return redirect('register')
+
+        # verifier le nom d;utilisateur 
+        if User.objects.filter(username=username).exists():
+           messages.warning(request,"Cet nom d'utilisateur existe déjà !")
+           return redirect('register')
+
+        user = User(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+
+    return render(request,'registration/register.html')
+
+
+def DO_LOGIN(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+		
+        user = EmailBackEnd.authenticate(request,
+                                     username=email,
+                                     password=password)
+        if user!=None:
+           login(request,user)
+           return redirect('home')
+        else:
+           messages.error(request,'Email ou Mot de passe Incorrecte... !')
+           return redirect('login')
+        
+
+def PROFILE(request):
+    return render(request,'registration/profile.html')
+
+
+def PROFILE_UPDATE(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user_id = request.user.id
+
+        user = User.objects.get(id=user_id)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.username = username
+        user.email = email
+
+        if password != None and password != "":
+            user.set_password(password)
+        user.save()
+        messages.success(request,'Profil a été mis en jour avec succè. ')
+        return redirect('profile')
+
+
+
+    
